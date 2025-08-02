d49bd70a02461e70198b8a6dd25b7b81b019b519
hoanntf9 <hoanntf9@gmail.com>
2025-08-02 19:59:16 +0700

    refactor code

--- utils/toast.js
+++ utils/toast.js
@@ -1,16 +1,16 @@
 export function toast({
   text = "",
-  duration = 30000,
+  duration = 3000,
   gravity = "top",
   position = "center",
 }) {
   Toastify({
     text,
-    duration, // thời gian hiển thị (ms)
-    close: true, // hiển thị nút đóng
-    gravity, // vị trí: "top" hoặc "bottom"
-    position, // vị trí: "left", "center" hoặc "right"
-    stopOnFocus: true, // dừng đếm ngược khi di chuột vào
+    duration,
+    close: true,
+    gravity,
+    position,
+    stopOnFocus: true,
     style: {
       background: "linear-gradient(to right, #1db954, #88f4ae)",
       minWidth: "200px",
